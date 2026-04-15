9b6603211fba38510ed828115e63dd38e76f2f86
CrunchyToothpicks100 <wolfywoofmc@gmail.com>
2026-04-14 22:24:44 -0500

    Update test cases with external_id

--- testing/tc01.sql
+++ testing/tc01.sql
@@ -1,4 +1,4 @@
 -- tc01
 -- Validate NOT NULL Constraint on Candidate Email
-INSERT INTO base_candidate (email, first_name, last_name, phone)
-VALUES (NULL, 'Test', 'User', '555-9999');
+INSERT INTO base_candidate (external_id, email, first_name, last_name, phone)
+VALUES (gen_random_uuid(), NULL, 'Test', 'User', '555-9999');
--- testing/tc05.sql
+++ testing/tc05.sql
@@ -1,8 +1,8 @@
 -- tc05
 -- Validate Cascade Delete Behavior
 -- Create test candidate
-INSERT INTO base_candidate (email, first_name, last_name, phone)
-VALUES ('cascade@test.com', 'Cascade', 'Test', '555-0000');
+INSERT INTO base_candidate (external_id, email, first_name, last_name, phone)
+VALUES (gen_random_uuid(), 'cascade@test.com', 'Cascade', 'Test', '555-0000');
 
 -- Create submission tied to that candidate (fields not included are allowed to be null and blank in models.py)
 INSERT INTO base_submission (candidate_id, status, created_at)
