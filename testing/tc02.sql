b815149817ec84f2dd20981715f87e0f83057607
CrunchyToothpicks100 <wolfywoofmc@gmail.com>
2026-04-15 19:57:12 -0500

    Update candidate test cases with likedin_url

--- testing/tc02.sql
+++ testing/tc02.sql
@@ -1,4 +1,4 @@
 -- tc02
 -- Validate Email Format Constraint
-INSERT INTO base_candidate (external_id, email, first_name, last_name, phone)
-VALUES (gen_random_uuid(), 'invalidemail.com', 'Bad', 'Email', '555-9999');
+INSERT INTO base_candidate (external_id, email, first_name, last_name, phone, linkedin_url, bio)
+VALUES (gen_random_uuid(), 'invalidemail.com', 'Bad', 'Email', '555-9999', '', '');
